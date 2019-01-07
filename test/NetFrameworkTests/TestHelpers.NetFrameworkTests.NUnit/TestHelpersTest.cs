c5c5395ae9a0107a3beb54a38eca29acffe02fd9
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2019-01-06 23:13:16 -0800

    Added NUnit .NET Framework tests

---
+++ test/NetFrameworkTests/TestHelpers.NetFrameworkTests.NUnit/TestHelpersTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHelpers.NetFrameworkTests.NUnit
+{
+    [TestFixture]
+    public class TestHelpersTest
+    {
+        readonly TestFileHelper _testHelper = new TestFileHelper();
+
+        [SetUp]
+        public void Setup()
+        {
+            _testHelper.Configuration.LogWriter = new DefaultTestLogWriter();
+        }
+
+        [Test]
+        public void CanOpenFile()
+        {
+            string projectDirectory = _testHelper.ProjectDirectory.FullName;
+            string fullFile = Path.Combine(projectDirectory, "test.txt");
+
+            Assert.IsTrue(File.Exists(fullFile));
+        }
+
+        [Test]
+        public void DetectNUnitFramework()
+        {
+            Assert.AreEqual(TestFrameworks.NUnit, _testHelper.TestFramework);
+        }
+    }
+}
