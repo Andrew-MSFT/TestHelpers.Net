efe7c83e8b5873f901883bc0db87a5127cdfb408
Andrew Hall (DEVDIV) <andrehal@microsoft.com>
2019-01-06 23:48:47 -0800

    Added .NET Framework xUnit

---
+++ test/NetFrameworkTests/TestHelpers.NetFrameworkTests.NUnit/TestHelpers.NetFrameworkTests.xUnit/TestHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestHelpers.NetFrameworkTests.xUnit
+{
+    public class TestHelperTests
+    {
+        readonly TestFileHelper _testHelper = new TestFileHelper();
+
+        [Fact]
+        public void CanOpenFile()
+        {
+            const string Expected = "Hello xUnit .NET Framework!";
+            string projectDirectory = _testHelper.ProjectDirectory.FullName;
+            string contents;
+            using (StreamReader sr = _testHelper.OpenFile(@"Data\data.txt"))
+            {
+                contents = sr.ReadToEnd();
+            }
+
+            Assert.Equal(Expected, contents);
+        }
+
+        [Fact]
+        public void DetectNUnitFramework()
+        {
+            Assert.Equal(TestFrameworks.xUnit, _testHelper.TestFramework);
+        }
+    }
+}
